ec35ee9b69943f3bdb2324b4a67854757907191f
Steven Ross <ross.steven@me.com>
2020-04-11 13:21:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -13,5 +13,5 @@ end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  {id: 69420}# return a hash with a key :id assigned to the provided number
 end
